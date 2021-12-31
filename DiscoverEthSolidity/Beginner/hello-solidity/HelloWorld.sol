0ea2c2bcef80f6faad0e5f0a6e42ccec41898c0d
clippefuentes <clippefuentes@gmail.com>
2021-12-30 21:16:45 -0700

    initial commit

---
+++ DiscoverEthSolidity/Beginner/hello-solidity/HelloWorld.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+  string myName = "Tristan";
+  
+  // Add this function:
+  function getMyName() public view returns(string memory) {
+    return myName;
+  }
+  
+  function changeMyName(string memory _newName) public {
+    myName = _newName;
+  }
+}
